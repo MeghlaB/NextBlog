cb9792e464d9500e91153af803b8d2a5b099b08a
MeghlaB <meghlab964@gmail.com>
2025-01-23 16:19:30 +0600

    frist commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/Components/Header";
+import Foote from "@/Components/Foote";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Header></Header>
+       <main className="min-h-screen"> {children}</main>
+       <Foote></Foote>
       </body>
     </html>
   );
